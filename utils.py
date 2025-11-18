fc1297f17caa0b55b62d7f7d92e7a4ee1bdbff61
Lee Si Woong <silverttthin@naver.com>
2025-11-19 00:51:23 +0900

    feat: 유틸에 top10 가져오기 추가

--- utils.py
+++ utils.py
@@ -2,11 +2,13 @@ from dotenv import load_dotenv
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-
+import requests
+from pprint import pprint
 
 load_dotenv()
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
+
 
 
 def get_song_info(track_id):
@@ -21,9 +23,28 @@ def get_song_info(track_id):
     artist_name = track['artists'][0]['name']
     artist_detail_url = track['artists'][0]['external_urls']['spotify']
     album_image_url = track['album']['images'][1].get('url', None)
+    song_detail_url = track['external_urls']['spotify']
 
     return {
         'artist_name': artist_name,
         'artist_detail_url': artist_detail_url,
-        'album_image_url': album_image_url
+        'album_image_url': album_image_url,
+        'song_detail_url': song_detail_url
     }
+
+
+def get_chart_from_spotify():
+    url = 'https://charts-spotify-com-service.spotify.com/public/v0/charts'
+
+    response = requests.get(url)
+    chart = []
+    for idx, entry in enumerate(response.json()['chartEntryViewResponses'][0]['entries'], start=1):
+        chart.append({
+            "Rank": idx,
+            "Artist": ', '.join([artist['name'] for artist in entry['trackMetadata']['artists']]),
+            "TrackName": entry['trackMetadata']['trackName'],
+        })
+        if idx == 10:
+            break
+    
+    return chart
